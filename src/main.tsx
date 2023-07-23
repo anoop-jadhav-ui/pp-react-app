e6219fc7d826e92d7ac62810bca37b32b7fc30e9
Anoop Jadhav <anoop.jadhav@thoughtworks.com>
2023-07-23 14:21:17 +0530

    import fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
+
 const theme = createTheme({
   typography: {
-    fontFamily: "Poppins, sans-serif",
+    fontFamily: "poppins, sans-serif",
   },
   components: {
     MuiButton: {
