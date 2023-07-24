d8529a02dee9dd7a6ec75aa2451b7ed313609fd9
Anoop Jadhav <anoop.jadhav@thoughtworks.com>
2023-07-24 13:17:35 +0530

    update logo path

--- src/components/atoms/Header/Header.tsx
+++ src/components/atoms/Header/Header.tsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material";
 import styles from "./Header.module.css";
+import logo from "../../../assets/logo/logo-colored.svg";
 
 function Header() {
   return (
     <Typography variant="h3" component="h1" textAlign="center">
       <div className={styles.logo}>
-        <img src="/src/assets/logo/logo-colored.svg" alt="logo" width="300px" />
+        <img src={logo} alt="logo" width="300px" />
       </div>
     </Typography>
   );
