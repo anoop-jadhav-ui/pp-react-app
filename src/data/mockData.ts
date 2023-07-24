2b95417d1b0aa4e468f99e9dd9b452aec0c91d26
Anoop Jadhav <anoop.jadhav@thoughtworks.com>
2023-07-24 13:18:36 +0530

    update mock data

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -4,9 +4,4 @@ export const mockTeamMembers = [
   { id: 2, name: "Harshada Raut", color: "tomato" },
   { id: 3, name: "Rishab Baid", color: "lightsalmon" },
   { id: 4, name: "Srujan Keskar", color: "plum" },
-  { id: 5, name: "John Doe", color: "lightcyan" },
-  { id: 6, name: "Jane Smith", color: "thistle" },
-  { id: 7, name: "Mike Johnson", color: "lightseagreen" },
-  { id: 8, name: "Emily Brown", color: "chocolate" },
-  { id: 9, name: "Chris Lee", color: "orchid" },
 ];
