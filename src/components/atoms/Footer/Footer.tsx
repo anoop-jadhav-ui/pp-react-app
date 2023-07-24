80de6e6af270cdd8a9c9b3eb783e60a2c21c752c
Anoop Jadhav <anoop.jadhav@thoughtworks.com>
2023-07-24 13:25:58 +0530

    update logo and footer

--- src/components/atoms/Footer/Footer.tsx
+++ src/components/atoms/Footer/Footer.tsx
@@ -2,10 +2,11 @@ import { Stack, Typography } from "@mui/material";
 
 function Footer() {
   return (
-    <Stack alignItems="center">
+    <Stack alignItems="center" sx={{ mb: 4 }}>
       <Typography textAlign="center" variant="caption">
-        This project is still work in progress. Most of the features might not
-        be available.
+        This project is still work in development. There is some default data
+        added, feel free to clear it and new members. The added data is not
+        stored anywhere as of now.
       </Typography>
     </Stack>
   );
